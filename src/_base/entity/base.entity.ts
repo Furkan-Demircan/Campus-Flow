db2d701efe73cafeb91a8cc0a44f9f81c21ccd17
Furkan Demircan <goooglenudle@gmail.com>
2025-07-21 14:49:47 +0300

    feat(entity): create BaseEntity class with common timestamp fields

---
+++ src/_base/entity/base.entity.ts
@@ -0,0 +1,27 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamp',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
--- src/_common/config/database.config.ts
+++ src/_common/config/database.config.ts
@@ -8,6 +8,6 @@ export default registerAs('database', () => ({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [entities],
+  entities: entities,
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
 }));
